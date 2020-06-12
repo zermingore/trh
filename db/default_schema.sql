83eed7faab3d13a35859111f4637352a04c261d7
Jean-Philippe Quenord <jp.quenord+git@gmail.com>
2020-06-12 15:59:58 +0200

    [DB] Extracted default values in the dedicated file

--- db/default_schema.sql
+++ db/default_schema.sql
@@ -11,27 +11,13 @@ CREATE TABLE categories(
   name TEXT
 );
 
-INSERT INTO categories(id, name) VALUES(1,  "unknown");
-INSERT INTO categories(id, name) VALUES(2,  "noun_masculine");
-INSERT INTO categories(id, name) VALUES(3,  "noun_feminine");
-INSERT INTO categories(id, name) VALUES(4,  "noun_neutral");
-INSERT INTO categories(id, name) VALUES(5,  "noun_plural");
-INSERT INTO categories(id, name) VALUES(6,  "pronoun");
-INSERT INTO categories(id, name) VALUES(7,  "verb");
-INSERT INTO categories(id, name) VALUES(8,  "adjective");
-INSERT INTO categories(id, name) VALUES(9,  "adverb");
-INSERT INTO categories(id, name) VALUES(10, "conjunction");
-INSERT INTO categories(id, name) VALUES(11, "preposition");
-INSERT INTO categories(id, name) VALUES(12, "interjection");
-INSERT INTO categories(id, name) VALUES(13, "sentence");
-INSERT INTO categories(id, name) VALUES(14, "abreviation");
-
 
 
 CREATE TABLE languages(
   id INTEGER PRIMARY KEY,
   name TEXT
 );
+
 
 
 CREATE table words(
--- db/default_values.sql
+++ db/default_values.sql
@@ -1,1 +1,16 @@
-INSERT INTO languages(name) VALUES("English");
+INSERT INTO languages(id, name) VALUES(1, "English");
+
+INSERT INTO categories(id, name) VALUES(1,  "unknown");
+INSERT INTO categories(id, name) VALUES(2,  "noun_masculine");
+INSERT INTO categories(id, name) VALUES(3,  "noun_feminine");
+INSERT INTO categories(id, name) VALUES(4,  "noun_neutral");
+INSERT INTO categories(id, name) VALUES(5,  "noun_plural");
+INSERT INTO categories(id, name) VALUES(6,  "pronoun");
+INSERT INTO categories(id, name) VALUES(7,  "verb");
+INSERT INTO categories(id, name) VALUES(8,  "adjective");
+INSERT INTO categories(id, name) VALUES(9,  "adverb");
+INSERT INTO categories(id, name) VALUES(10, "conjunction");
+INSERT INTO categories(id, name) VALUES(11, "preposition");
+INSERT INTO categories(id, name) VALUES(12, "interjection");
+INSERT INTO categories(id, name) VALUES(13, "sentence");
+INSERT INTO categories(id, name) VALUES(14, "abreviation");
