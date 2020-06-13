0d79088ee0a74eda12031a29e72b2eea7ab47757
Jean-Philippe Quenord <jp.quenord+git@gmail.com>
2020-06-13 11:33:45 +0200

    [DB] Added 'plurals' and 'translations' tables in the schema

--- db/default_schema.sql
+++ db/default_schema.sql
@@ -6,6 +6,7 @@ CREATE table version(major, minor, patch);
 INSERT INTO version VALUES(0, 0, 1);
 
 
+
 CREATE TABLE categories(
   id INTEGER PRIMARY KEY,
   name TEXT
@@ -20,7 +21,26 @@ CREATE TABLE languages(
 
 
 
-CREATE table words(
+CREATE TABLE translations(
+  id_word_src INTEGER NOT NULL,
+  id_word_dst INTEGER,
+
+  FOREIGN KEY(id_word_src) REFERENCES words(id)
+  FOREIGN KEY(id_word_dst) REFERENCES words(id)
+);
+
+
+
+CREATE TABLE plurals(
+  id_word INTEGER NOT NULL,
+  name TEXT,
+
+  FOREIGN KEY(id_word) REFERENCES words(id)
+);
+
+
+
+CREATE TABLE words(
   id INTEGER PRIMARY KEY,
   id_language INTEGER NOT NULL,
   id_category INTEGER NOT NULL,
