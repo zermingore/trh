28784bb4be4b3c155e9c22f7d3a5c1f85892c2a9
Jean-Philippe Quenord <jp.quenord+git@gmail.com>
2020-08-29 10:44:00 +0200

    [Log] Handling colored prints

--- src/Log.cc
+++ src/Log.cc
@@ -5,18 +5,61 @@
 Gtk::TextView* Log::_view;
 Glib::RefPtr<Gtk::TextBuffer> Log::_buffer;
 
+Gtk::TextIter Log::_bufIter;
+Glib::RefPtr<Gtk::TextTagTable> Log::_reftagtable;
+Glib::RefPtr<Gtk::TextTag> Log::_noticeTag;
+Glib::RefPtr<Gtk::TextTag> Log::_warningTag;
+Glib::RefPtr<Gtk::TextTag> Log::_errorTag;
+
 
 
 void Log::init(Glib::RefPtr<Gtk::Builder> builder)
 {
   builder->get_widget("console", _view);
-  _buffer = Gtk::TextBuffer::create();
+
+  _reftagtable = Gtk::TextBuffer::TagTable::create();
+
+  _noticeTag = Gtk::TextBuffer::Tag::create();
+  _noticeTag->property_foreground() = "blue";
+  _reftagtable->add(_noticeTag);
+
+  _warningTag = Gtk::TextBuffer::Tag::create();
+  _warningTag->property_foreground() = "orange";
+  _reftagtable->add(_warningTag);
+
+  _errorTag = Gtk::TextBuffer::Tag::create();
+  _errorTag->property_foreground() = "red";
+  _reftagtable->add(_errorTag);
+
+  _buffer = Gtk::TextBuffer::create(_reftagtable);
+  _view->set_monospace(true);
+
   _view->set_buffer(_buffer);
+
+  _bufIter = _buffer->get_iter_at_offset(0);
 }
 
 
 
 void Log::print(const char* str)
 {
-  _buffer->insert_at_cursor(str);
+  _bufIter = _buffer->insert(_bufIter, str);
+}
+
+
+void Log::error(const char* str)
+{
+  _bufIter = _buffer->insert_with_tag(_bufIter, str, _errorTag);
+}
+
+
+void Log::warning(const char* str)
+{
+  _bufIter = _buffer->insert_with_tag(_bufIter, str, _warningTag);
+}
+
+
+void Log::notice(const char* str)
+{
+  _bufIter = _buffer->insert_with_tag(_bufIter, str, _noticeTag);
 }
--- src/Log.hh
+++ src/Log.hh
@@ -1,3 +1,7 @@
+#ifndef LOG_HH_
+# define LOG_HH_
+
+# include <iterator>
 # include <gtkmm.h>
 
 
@@ -8,9 +12,20 @@ public:
   static void init(Glib::RefPtr<Gtk::Builder> builder);
 
   static void print(const char* str);
+  static void notice(const char* str);
+  static void warning(const char* str);
+  static void error(const char* str);
 
 
 private:
   static Gtk::TextView* _view;
   static Glib::RefPtr<Gtk::TextBuffer> _buffer;
+
+  static Gtk::TextIter _bufIter;
+  static Glib::RefPtr<Gtk::TextTagTable> _reftagtable;
+  static Glib::RefPtr<Gtk::TextTag> _noticeTag;
+  static Glib::RefPtr<Gtk::TextTag> _warningTag;
+  static Glib::RefPtr<Gtk::TextTag> _errorTag;
 };
+
+#endif // LOG_HH_
